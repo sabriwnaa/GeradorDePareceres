7a543984c7e14db843de1b17c35e384ce05e7b11
sabriwnaa <sabrinahm2006@gmail.com>
2025-06-19 23:06:06 -0300

    funcionando banco de dados

--- geradorpareceres.sql
+++ geradorpareceres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20/06/2025 às 01:31
+-- Tempo de geração: 20/06/2025 às 03:58
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
